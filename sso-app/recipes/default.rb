b22a66eaf53102a501fa5a9e286c11803cac4948
ot-nemoto <nemoto@opentone.co.jp>
2019-03-03 07:43:43 +0000

    bundlerのバージョン指定(1.17.3)

--- sso-app/recipes/default.rb
+++ sso-app/recipes/default.rb
@@ -18,8 +18,7 @@ execute "ruby-install" do
   user "root"
   command <<-EOH
     amazon-linux-extras install -y ruby2.4 && \
-    gem install bundler && \
-    gem update --system
+    gem install bundler -v 1.17.3
   EOH
   action :run
 end
